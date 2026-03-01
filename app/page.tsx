959a02b0bb01517634935e7669525d13ef996ec9
Aman Nagar <nagarsheetal48@gmail.com>
2026-03-01 10:35:28 +0530

    commit in feature login

--- app/page.tsx
+++ app/page.tsx
@@ -34,6 +34,8 @@ export default function Home() {
             center.
           </p>
         </div>
+        <div className="p-2 rounded-xl border bg-pink-500" >This is from first commit - 01</div>
+        <div className="p-2 rounded-xl border bg-pink-500" >This is from feature-login commit - 02</div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
